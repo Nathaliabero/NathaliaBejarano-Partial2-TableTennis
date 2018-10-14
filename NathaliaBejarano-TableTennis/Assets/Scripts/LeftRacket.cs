2ffd94945b261246297fa670aeeaa8cd98551ba9
Nathaliabero <nataliabejarano243@gmail.com>
2018-10-13 22:33:55 -0500

    Corregido

---
+++ NathaliaBejarano-TableTennis/Assets/Scripts/LeftRacket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRacket : MonoBehaviour {
+
+    public float MotionSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.D)) {
+
+            //Esta instrucción mueve el GameObject hacia su izquierda        
+            this.transform.Translate(Vector3.forward * MotionSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+
+            //Esta instrucción mueve el GameObject hacia su derecha       
+            this.transform.Translate(Vector3.back * MotionSpeed);
+        }
+
+        if (Input.GetKey (KeyCode.W)) {
+
+            this.transform.Translate(Vector3.up * MotionSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+
+            this.transform.Translate(Vector3.down * MotionSpeed);
+        }
+    }
+}
---
+++ NathaliaBejarano-TableTennis/Assets/Scripts/RightRacket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightRacket : MonoBehaviour {
+
+    public float MotionSpeed;
+        
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+     
+            this.transform.Translate(Vector3.forward * MotionSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+      
+            this.transform.Translate(Vector3.back * MotionSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+       
+            this.transform.Translate(Vector3.up * MotionSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+       
+            this.transform.Translate(Vector3.down * MotionSpeed);
+        }
+    }
+}
